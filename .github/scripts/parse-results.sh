5b1f07997daa058df17af910d5bf13154446c9d2
Alastair Brown <alastair.browen@sage.com>
2024-01-15 16:17:36 +0000

    Clean debug steps out of script

--- .github/scripts/parse-results.sh
+++ .github/scripts/parse-results.sh
@@ -1,16 +1,10 @@
 if [[ -f apexTestResults/test/test-result.txt ]]; then #1
     echo "Results found"
     # get list of failed tests | trim for class.test name only
-    step1=$(grep -w 'Fail' apexTestResults/test/test-result.txt)
-    echo "done step 1"
-    echo "$step1"
-    step2=$(grep -F -e 'fHCM2' apexTestResults/test/test-result.txt)
-    echo "done step 2"
-    echo "$step2"
     grep -v --line-buffered "Fail" apexTestResults/test/test-result.txt >> apexTestResults/test/step1.txt
-    echo apexTestResults/test/step1.txt
+    echo "$(<apexTestResults/test/step1.txt)"
     grep -v --line-buffered "fHCM2" apexTestResults/test/step1.txt >> apexTestResults/test/step2.txt
-    echo apexTestResults/test/step2.txt
+    echo "$(<apexTestResults/test/step2.txt)"
     failed=$(awk {'print $1'} -f apexTestResults/test/step2.txt)            
     echo "Parsed test results"
     echo "$failed"
